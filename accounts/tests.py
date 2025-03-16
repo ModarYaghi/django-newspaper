7f87a6415560b5c6033a16e1c58c67ec2b0bef33
ModarYaghi <mfyaghi81@gmail.com>
2025-03-16 08:02:45 +0300

    Signup page: add view, URL, and template test

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,6 +6,7 @@ creation operate as expected.
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
 #
@@ -60,3 +61,30 @@ class UsersManagersTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpPageTest(TestCase):
+
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)  # type: ignore
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)  # type: ignore
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@email.com",
+                "password1": "testpass123",
+                "password2": "testpass123",
+            },
+        )
+        self.assertEqual(response.status_code, 302)  # type: ignore
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser@email.com")
