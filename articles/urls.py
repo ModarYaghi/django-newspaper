9b98597dda8081021bfb270597e27f8234d859d7
ModarYaghi <mfyaghi81@gmail.com>
2025-03-18 06:30:25 +0300

    Articles urls: typo correction

--- articles/urls.py
+++ articles/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 urlpatterns = [
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
-    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="articel_edit"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
     path("", ArticleListView.as_view(), name="article_list"),
 ]
