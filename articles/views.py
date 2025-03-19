1bb2a8ade5b77669d327343f1c432216f43bcc11
ModarYaghi <mfyaghi81@gmail.com>
2025-03-19 06:11:22 +0300

    Articles views: add form_valid method to assign logged-in user as the author

--- articles/views.py
+++ articles/views.py
@@ -18,8 +18,29 @@ class ArticleCreateView(CreateView):
     fields = (
         "title",
         "body",
-        "author",
+        # "author",  # this commented out to make author is the logged in user.
     )
+
+    def form_valid(self, form):
+        """
+        Assign the logged -in user as the author and save the article.
+
+        This method is invoked when the submitted form is valid. It automatically
+        sets the 'author' field of the Article instance (available as form.instance)
+        to the current user (self.request.user). This ensures that the article is
+        properly associated with the user who created it without requiring manual
+        input for the author field.
+        Finally, it delegates the rest of the form processing (saving the instance and redirecting)
+        to the parent class's form_valid method.
+
+        :param form: A valid form instance containing the data for the new article.
+        :type form: django.forms.ModelForm
+        :return: An HTTP redirect response after the article is successfully saved.
+        :rtype: django.http.HttpResponse
+        """
+
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class ArticleListView(ListView):
